1e8f5dcbab4b7b467e9ab62072da37822be2cf8a
juanjosedelacruzamarillo-ai <juanjosedelacruzamarillo@gmail.com>
2026-06-02 11:04:04 +0200

    añadir constructores por defecto y con parametro

--- actividad01/Videojuego.java
+++ actividad01/Videojuego.java
@@ -6,4 +6,18 @@ public class Videojuego {
     private String genero;
     private double precio;
     private int lanzamientoAnio;
+
+    public Videojuego() {
+        this.titulo = "";
+        this.genero = "";
+        this.precio = 0.0;
+        this.lanzamientoAnio = 0;
+    }
+
+    public Videojuego(String titulo, String genero, double precio, int lanzamientoAnio) {
+        this.titulo = titulo;
+        this.genero = genero;
+        this.precio = precio;
+        this.lanzamientoAnio = lanzamientoAnio;
+    }
 }
