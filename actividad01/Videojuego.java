4dd32a0b8db3fe1bd8bb66a587a9f08867291838
cuentaparamierdas17-star <cuentaparamierdas17@gmail.com>
2026-06-02 10:48:34 +0200

    Añadida clase Videojuego con atributos básicos

---
+++ actividad01/Videojuego.java
@@ -0,0 +1,9 @@
+package com.mycompany.actividad01;
+
+public class Videojuego {
+
+    private String titulo;
+    private String genero;
+    private double precio;
+    private int lanzamientoAnio;
+}
