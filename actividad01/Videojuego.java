576c953c320d2af1dfe85874805debb6ea052913
juanjosedelacruzamarillo-ai <juanjosedelacruzamarillo@gmail.com>
2026-06-04 10:34:38 +0200

    Javadoc

--- actividad01/Videojuego.java
+++ actividad01/Videojuego.java
@@ -1,5 +1,13 @@
 package com.mycompany.actividad01;
 
+/**
+ * 
+ * @author PC-03
+ * 
+ * @version 4/6/2026
+ * 
+ */
+
 public class Videojuego {
 
     /**
@@ -35,37 +43,68 @@ public class Videojuego {
         this.lanzamientoAnio = lanzamientoAnio;
     }
 
+    //Metodos Getters y Setters
     /**
-     * Metodos Getters y Setters
+     *
+     * @param titulo
      */
     public void setTitulo(String titulo) {
         this.titulo = titulo;
     }
 
+    /**
+     *
+     * @param genero
+     */
     public void setGenero(String genero) {
         this.genero = genero;
     }
 
+    /**
+     *
+     * @param precio
+     */
     public void setPrecio(double precio) {
         this.precio = precio;
     }
+
+    /**
+     *
+     * @param lanzamientoAnio
+     */
 
     public void setLanzamientoAnio(int lanzamientoAnio) {
         this.lanzamientoAnio = lanzamientoAnio;
     }
 
+    /**
+     *
+     * @return
+     */
     public String getTitulo() {
         return titulo;
     }
 
+    /**
+     *
+     * @return
+     */
     public String getGenero() {
         return genero;
     }
 
+    /**
+     *
+     * @return
+     */
     public double getPrecio() {
         return precio;
     }
 
+    /**
+     *
+     * @return
+     */
     public int getLanzamientoAnio() {
         return lanzamientoAnio;
     }
