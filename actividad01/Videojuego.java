934928a09cd3b44967b102b6d36bacc51ee24204
cuentaparamierdas17-star <cuentaparamierdas17@gmail.com>
2026-06-04 10:31:27 +0200

    Añadido comentarios

--- actividad01/Videojuego.java
+++ actividad01/Videojuego.java
@@ -2,11 +2,17 @@ package com.mycompany.actividad01;
 
 public class Videojuego {
 
+    /**
+     * Los atributos titulo,genero,precio y lanzamientoAnio
+     */
     private String titulo;
     private String genero;
     private double precio;
     private int lanzamientoAnio;
 
+    /**
+     * Contructor sin parametos
+     */
     public Videojuego() {
         this.titulo = "";
         this.genero = "";
@@ -14,6 +20,14 @@ public class Videojuego {
         this.lanzamientoAnio = 0;
     }
 
+    /**
+     * Constructor con parametros
+     *
+     * @param titulo
+     * @param genero
+     * @param precio
+     * @param lanzamientoAnio
+     */
     public Videojuego(String titulo, String genero, double precio, int lanzamientoAnio) {
         this.titulo = titulo;
         this.genero = genero;
@@ -21,6 +35,9 @@ public class Videojuego {
         this.lanzamientoAnio = lanzamientoAnio;
     }
 
+    /**
+     * Metodos Getters y Setters
+     */
     public void setTitulo(String titulo) {
         this.titulo = titulo;
     }
